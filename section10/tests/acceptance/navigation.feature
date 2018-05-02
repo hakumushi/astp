a57eca590d180ffb76086e1cdcd669e30558fe8d
francapr <pr.francaf@gmail.com>
2018-05-02 01:13:15 -0300

    The end of the course
    The end of the course

--- section10/tests/acceptance/navigation.feature
+++ section10/tests/acceptance/navigation.feature
@@ -2,14 +2,12 @@ Feature: Test navigation between pages
   We can have a longer description
   That can span a few lines
 
-
   Scenario: Homepage can go to Blog
     Given I am on the homepage
-    When I click on the link with id "blog-link"
+    When I click on the "Go to blog" link
     Then I am on the blog page
-
 
     Scenario: Homepage can go to Blog
       Given I am on the blog page
-      When I click on the link with id "home-link"
+      When I click on the "Go to home" link
       Then I am on the homepage
---
+++ section10/tests/acceptance/page_model/new_post_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from tests.acceptance.page_model.base_page import BasePage
+from tests.acceptance.locators.new_post_page import NewPostPageLocators
+
+class NewPostPage(BasePage):
+    @property
+    def url(self):
+        return super(NewPostPage, self).url + '/post'
+
+    @property
+    def form(self):
+        return self.driver.find_element(*NewPostPageLocators.NEW_POST_FORM)
+
+    @property
+    def submit_button(self):
+        return self.driver.find_element(*NewPostPageLocators.SUBMIT_BUTTON)
+
+    def form_field(self, name):
+        return self.form.find_element(By.NAME, name)
+
